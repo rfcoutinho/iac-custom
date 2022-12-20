0b95a272e154c1cbabb011b60244327c401356bf
rfcoutinho <rafael@myself.com>
2022-12-19 22:48:28 -0300

    add artifacts

---
+++ artifacts/scripts/install-jenkins.sh
@@ -0,0 +1,10 @@
+  #!/usr/bin/env bash
+
+  sudo apt-get update -y
+  sudo apt install default-jre -y
+  wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+  sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+  sudo apt update
+  sudo apt install jenkins -y
+  sudo systemctl start jenkins
+  sudo install maven -y
